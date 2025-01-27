34adca5e86c1b138cc0fb1e6d7e750f897aa5c58
piet <pprotassow@posteo.de>
2025-01-27 19:09:49 +0100

    int to slug

--- blog/models.py
+++ blog/models.py
@@ -1,17 +1,25 @@
 from django.db import models
 from django.urls import reverse
+from django.utils.text import slugify
 
 class Post(models.Model):
     title = models.CharField(max_length=200)
-    author = models.ForeignKey("auth.User", on_delete=models.CASCADE) # all posts that are related to specific user
-                                                        # and this is deleted then all their associated posts will be
-                                                        # deleted
+    slug = models.SlugField(unique=True, max_length=200, blank=True)
+    author = models.ForeignKey("auth.User", on_delete=models.CASCADE) 
     body = models.TextField()
+
+    def save(self, *args, **kwargs):
+        counter = 1
+        base_slug = f"{slugify(self.title)}-{counter}"
+        while Post.objects.filter(slug=base_slug).exists():
+            counter += 1
+        self.slug = f"{slugify(self.title)}-{counter}"
+        return super().save(*args, **kwargs)
 
     def __str__(self):
         return self.title
 
     def get_absolute_url(self):
-        return reverse('post_detail', kwargs={"pk": self.id})
+        return reverse('post_detail', kwargs={"slug": self.slug})
 
 
--- blog/urls.py
+++ blog/urls.py
@@ -3,5 +3,6 @@ from .views import BlogListView, BlogDetailTemplateView, BlogDetailView, blog_de
 
 urlpatterns = [
     path("", BlogListView.as_view(), name="home"),
-    path("post/<int:pk>/", BlogDetailTemplateView.as_view(), name="post_detail")
+    # path("post/<int:pk>/", BlogDetailTemplateView.as_view(), name="post_detail")
+    path("post/<slug:slug>/", BlogDetailView.as_view(), name="post_detail")
 ]
