26128f49fa96f6a8f5e3ba7657dff21b47f5cf70
piet <pprotassow@posteo.de>
2025-02-06 16:39:44 +0100

    fix

--- django_project/urls.py
+++ django_project/urls.py
@@ -19,8 +19,8 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("accounts/", include("allauth.urls")),
-    # path("my_accounts/", include("django.contrib.auth.urls")),
     path("accounts/", include("accounts.urls")),
+    path("accounts/", include("allauth.urls")),
+    path("my_accounts/", include("django.contrib.auth.urls")),
     path('', include('blog.urls')),
 ]
